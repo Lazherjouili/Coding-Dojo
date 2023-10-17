95f19e7727166edca9cb6054a406fd4139704990
Lazherjouili <lazher.jouili.job@gmail.com>
2023-10-17 15:06:58 +0100

    COMMIT

---
+++ Programming Basics/Week 1/Day 2/Core/code.js
@@ -0,0 +1,24 @@
+var age=9;
+var height=50;
+if(height>=42){
+    console.log("Get on that ride, kiddo!");
+}
+else{
+    console.log("Sorry kiddo. Maybe next year");
+}
+
+
+
+if(height>=42 && age>=10){
+    console.log("Get on that ride, kiddo!");
+}
+else {
+    console.log("Sorry kiddo. Maybe next year");
+}
+
+if(height>=42 || age>=10){
+    console.log("Get on that ride, kiddo!");
+}
+else {
+    console.log("Sorry kiddo. Maybe next year");
+}
