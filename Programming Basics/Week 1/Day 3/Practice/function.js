bb969c6c062329514d7404faba8c3ddec2874671
Lazherjouili <lazher.jouili.job@gmail.com>
2023-10-20 15:39:15 +0100

    function

---
+++ Programming Basics/Week 1/Day 3/Practice/function.js
@@ -0,0 +1,12 @@
+function greet(name,hour) {
+    if(name=="Count Dooku"){
+        console.log("I'm coming for you, Dooku!")
+        
+    }
+    else{
+        console.log("Good day,",name,hour);
+    }
+}
+greet("Count Dooku","09:30")
+
+
