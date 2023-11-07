7d6a1066d88e6cf34f7977bba0896add9db0efef
Lazherjouili <lazher.jouili.job@gmail.com>
2023-11-07 17:10:10 +0100

    page

---
+++ WebFundamentals/Week 2/Day 2/Practice/profile-page/script.js
@@ -0,0 +1,11 @@
+console.log("page loaded...");
+
+var rev=document.querySelector(".rev")
+function remove(){
+    rev.remove()
+}
+
+chan=document.querySelector(".card-body h1")
+function change(){
+   chan.innerText="Lazher jouili"
+}
