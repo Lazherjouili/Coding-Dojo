0272356580d2207cb1a13a936f6664d68d82504c
Lazherjouili <lazher.jouili.job@gmail.com>
2023-11-15 22:06:53 +0100

    bankaccount

---
+++ Python/Week 1/Day 2/Core/bankaccount.py
@@ -0,0 +1,41 @@
+class BankAccount:
+    list_of_accounts = []
+    # don't forget to add some default values for these parameters!
+    def __init__(self,name):
+        self.name=name 
+        self.int_rate=0.01
+        self.balance=0 
+        BankAccount.list_of_accounts.append(self)       
+    def deposit(self, amount):
+        self.balance+=amount
+        return self
+        
+    def withdraw(self, amount):
+        if self.balance>=amount:
+            self.balance-=amount
+        else:
+            print("insufficient funds: Charging a $5 fee")
+            self.balance-=5 
+        return self
+
+
+    def display_account_info(self):
+        print("======================")
+        print(f"Balance:{self.balance}")
+        return self
+        print("======================")
+
+
+    def yield_interest(self):
+        self.balance-=self.balance*self.int_rate
+        return self
+
+
+
+account1=BankAccount("lazher")
+account2=BankAccount("abdelkarim")
+
+print(account1)
+print(account2)
+account1.deposit(1000).deposit(1000).deposit(1000).withdraw(1500).yield_interest().display_account_info()
+account2.deposit(500).deposit(500).withdraw(300).withdraw(300).withdraw(300).withdraw(300).yield_interest().display_account_info()
